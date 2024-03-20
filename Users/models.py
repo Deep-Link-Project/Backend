e126a2ea8ad530b176d38704e99240e85506a8e5
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2024-03-20 15:11:22 +0330

    some restrictions on email

--- Users/models.py
+++ Users/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Member(models.Model):
 
     full_name = models.CharField(max_length=255,null=False)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
 
     def __str__(self) -> str:
         return self.full_name
